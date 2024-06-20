7914781c94db8d782329843afbe94813391b86f5
Trần Thẩm Anh Toàn <121923332+ttat288@users.noreply.github.com>
2024-06-20 14:07:18 +0700

    Added screen login, sign up, main menu, connect to monggoDb atlas

---
+++ Assets/Script/MainMenuSecond.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuSecond : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
